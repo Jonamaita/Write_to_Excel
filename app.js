e335f9c196b1584b5d83627a3f0cf0e15ded6b7d
Jonathan <jonamaita@gmail.com>
2018-05-15 16:18:47 -0400

    boton falla resuelta

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var app = express();
 var bodyParser = require('body-parser'); // Parser para recibir información del cliente http
 var Excel = require('exceljs'); // Modulo para escribir en excel
 var workbook = new Excel.Workbook(); // realizar una instancia
-var date = new Date();
 // Arcivhos estaticos
 app.use(express.static(__dirname + '/public'));
 //body parser para el fromulario
@@ -13,22 +12,25 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 //pagina principal
 app.get("/", function(req, res) {
-    res.sendfile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 //Petición formulario
-app.get("/datos_track", function(req, res) {
+app.post("/datos_track", function(req, res) {
     //console.log(req.query); //query cuando es get y body cuando es post
-    console.log(req.query.comentario);
-    var problema = req.query.problema;
-    var comentario = req.query.comentario;
+    console.log(req.body.comentario);
+    var problema = req.body.problema;
+    var comentario = req.body.comentario;
     escrbir_excel(problema, comentario);
-    res.sendfile(__dirname + '/public/index.html');
+    //res.sendFile(__dirname + '/public/succes.html');
+    res.send('form_sucess');
 });
+///////////
+
 //Escucha servidor
-app.listen(5555, () => {
+app.listen(5555,"192.168.1.114", () => {
     console.log("Servidor On");
 });
-
+/////////////////////////
 function escrbir_excel(problema, comentario) {
     workbook.xlsx.readFile('track_mantenimiento.xlsx').then(function() {
         var worksheet = workbook.getWorksheet("track_mantenimiento");
@@ -41,8 +43,11 @@ function escrbir_excel(problema, comentario) {
         var row = worksheet.getRow(i + 1);
         row.getCell(1).value = problema; //
         row.getCell(2).value = comentario;
+        var date = new Date();
         row.getCell(3).value = date;
-        row.getCell(4).value = date.getHours() + ":" + date.getMinutes();
+        hora = date.getHours() + ":" + date.getMinutes();
+        row.getCell(4).value = hora;
+        console.log(hora);
         //row.commit();
         return workbook.xlsx.writeFile('track_mantenimiento.xlsx');
     })
--- public/javascript.js
+++ public/javascript.js
@@ -1,14 +1,49 @@
 $(document).ready(function() {
-            $("#enviar-form").click(function() {
-                    if (validar_form) {
-                        $.get("datos_track", $('#form_exito').attr('class', "");
-                        }
-                    });
-            });
+    console.log("jquery On");
+    $("#enviar-form").click(function() {
+       validar_form();
+       
+        
+    });
+});
 
-        function validar_form() {
-            if (("#comentario").value() = "") {
-                console.log("Vacio");
-                return false;
-            } else {}
-        };
+function validar_form() {
+        var expreg=/\S/;
+        var valTextArea=document.getElementById('comentario').value;
+
+    if (!valTextArea.match(expreg)) {
+        alert("El campo de comentario no puede estar vacio.");
+        $("#comentario").focus();
+        $("#comentario").addClass("incorrecto")
+        return false;
+    } else {
+        $.ajax({
+            type: "POST",
+            url: "/datos_track",
+            data: $('#form_1').serialize(),
+            success: function(data) {
+                //funcion que se ejecutara en caso de que todo vaya bien
+                $('#res_form').addClass('correcto');
+                $('#form_success_error').removeClass('no-visible');
+                $('#res_form').html("Los datos se enviaron correctamente");
+                document.getElementById("form_1").reset();
+                $("#run_tf").removeClass("hide");
+                //document.getElementById("comentario").disabled = true;
+                setTimeout(function(){
+                    $("#comentario").addClass("hide");
+                },1000);
+               
+
+              
+                
+            },
+            error: function(data) {
+                //funcion que se ejecutara en caso de que se haya producido un fallo
+                $('#res_form').addClass('incorrecto');
+                $('#form_success_error').removeClass('no-visible');
+                $('#res_form').html("Los datos no se enviaron correctamente, intente nuevamente");
+                document.getElementById("form_1").reset();
+            }
+        });
+    }
+};
